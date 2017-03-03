b524640f749fe3735bcc9501fe8a0d89ec7dcd56
misha <chemike47@gmail.com>
2017-03-03 23:17:59 +0300

    Add tests for Dictionary

--- src/test/java/ru/spbau/mit/DictionaryTest.java
+++ src/test/java/ru/spbau/mit/DictionaryTest.java
@@ -1,29 +1,44 @@
 package ru.spbau.mit;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class DictionaryTest {
-    @Test
-    public void testSimplePutGet() {
-        Dictionary dict = instance();
-        assertNull(dict.get("abc"));
+    private Dictionary dictionary;
 
-        assertNull(dict.put("abc", "cde"));
-        assertEquals("cde", dict.get("abc"));
+    @Before
+    public void setUp() throws Exception {
+        dictionary = new DictionaryImpl();
     }
 
-    private static Dictionary instance() {
-        try {
-            return (Dictionary) Class.forName("ru.spbau.mit.DictionaryImpl").newInstance();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        throw new IllegalStateException("Error while class loading");
+    @Test
+    public void testPutGet() throws Exception {
+        assertNull(dictionary.get("abc"));
+        assertNull(dictionary.put("abc", "abc"));
+        assertEquals("abc", dictionary.put("abc", "def"));
+        assertEquals("def", dictionary.get("abc"));
+    }
+
+    @Test
+    public void testRemoveContains() throws Exception {
+        assertFalse(dictionary.contains("abc"));
+        dictionary.put("abc", "def");
+        assertTrue(dictionary.contains("abc"));
+        assertEquals("def", dictionary.remove("abc"));
+        assertFalse(dictionary.contains("abc"));
+    }
+
+    @Test
+    public void testSizeClear() throws Exception {
+        dictionary.put("abc", "123");
+        dictionary.put("def", "456");
+        dictionary.put("def", "789");
+        assertEquals(2, dictionary.size());
+        dictionary.remove("def");
+        assertEquals(1, dictionary.size());
+        dictionary.clear();
+        assertEquals(0, dictionary.size());
     }
 }
