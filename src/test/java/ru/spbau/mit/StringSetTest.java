f1fd6fa80f42994f073d25a7a27909ba4c28189d
Denis Zharkov <denis.zharkov@jetbrains.com>
2017-03-10 09:24:31 +0300

    Add task 03-serializable-string-set

--- src/test/java/ru/spbau/mit/StringSetTest.java
+++ src/test/java/ru/spbau/mit/StringSetTest.java
@@ -1,9 +1,12 @@
 package ru.spbau.mit;
 
+import static org.junit.Assert.*;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class StringSetTest {
 
@@ -15,6 +18,42 @@ public class StringSetTest {
         assertTrue(stringSet.contains("abc"));
         assertEquals(1, stringSet.size());
         assertEquals(1, stringSet.howManyStartsWithPrefix("abc"));
+    }
+
+    @Test
+    public void testSimpleSerialization() {
+        StringSet stringSet = instance();
+
+        assertTrue(stringSet.add("abc"));
+        assertTrue(stringSet.add("cde"));
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ((StreamSerializable) stringSet).serialize(outputStream);
+
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+        StringSet newStringSet = instance();
+        ((StreamSerializable) newStringSet).deserialize(inputStream);
+
+        assertTrue(newStringSet.contains("abc"));
+        assertTrue(newStringSet.contains("cde"));
+    }
+
+
+    @Test(expected=SerializationException.class)
+    public void testSimpleSerializationFails() {
+        StringSet stringSet = instance();
+
+        assertTrue(stringSet.add("abc"));
+        assertTrue(stringSet.add("cde"));
+
+        OutputStream outputStream = new OutputStream() {
+            @Override
+            public void write(int b) throws IOException {
+                throw new IOException("Fail");
+            }
+        };
+
+        ((StreamSerializable) stringSet).serialize(outputStream);
     }
 
     public static StringSet instance() {
