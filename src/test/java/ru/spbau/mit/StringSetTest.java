604b9df9133ea253f90edc18014be9b4afc83b0f
misha <chemike47@gmail.com>
2017-03-12 01:05:59 +0300

    Rewrite tests using nio.channels.Pipe

--- src/test/java/ru/spbau/mit/StringSetTest.java
+++ src/test/java/ru/spbau/mit/StringSetTest.java
@@ -2,69 +2,75 @@ package ru.spbau.mit;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import java.io.*;
+import java.nio.channels.Channels;
+import java.nio.channels.Pipe;
 
 public class StringSetTest {
     private StringSetImpl stringSet;
+    private StringSetImpl deserializedStringSet;
+    private InputStream input;
+    private OutputStream output;
 
     @Before
     public void setUp() throws Exception {
         stringSet = new StringSetImpl();
+        deserializedStringSet = new StringSetImpl();
+        final Pipe pipe = Pipe.open();
+        input = Channels.newInputStream(pipe.source());
+        output = Channels.newOutputStream(pipe.sink());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        input.close();
+        output.close();
     }
 
     @Test
-    public void tesSerializationSimple() {
+    public void testSerializationSimple() throws IOException {
         assertTrue(stringSet.add("abc"));
         assertTrue(stringSet.add("cde"));
-        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        stringSet.serialize(outputStream);
-
-        final ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
-        final StringSetImpl newStringSet = new StringSetImpl();
-        newStringSet.deserialize(inputStream);
-        assertTrue(newStringSet.contains("abc"));
-        assertTrue(newStringSet.contains("cde"));
+        stringSet.serialize(output);
+        output.close();
+        deserializedStringSet.deserialize(input);
+        assertTrue(deserializedStringSet.contains("abc"));
+        assertTrue(deserializedStringSet.contains("cde"));
+        assertEquals(stringSet.size(), deserializedStringSet.size());
     }
 
     @Test
-    public void testSerializationEmpty() {
-        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        stringSet.serialize(outputStream);
-
-        final ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
-        final StringSetImpl newStringSet = new StringSetImpl();
-        newStringSet.deserialize(inputStream);
-        assertEquals(0, newStringSet.size());
+    public void testSerializationEmpty() throws IOException {
+        stringSet.serialize(output);
+        output.close();
+        deserializedStringSet.deserialize(input);
+        assertEquals(stringSet.size(), deserializedStringSet.size());
     }
 
     @Test
-    public void tesSerializationDeep() {
+    public void testSerializationDeep() throws IOException {
         assertTrue(stringSet.add(""));
         assertTrue(stringSet.add("a"));
         assertTrue(stringSet.add("abc"));
-        final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        stringSet.serialize(outputStream);
-
-        final ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
-        final StringSetImpl newStringSet = new StringSetImpl();
-        newStringSet.deserialize(inputStream);
-        assertTrue(newStringSet.contains(""));
-        assertTrue(newStringSet.contains("a"));
-        assertFalse(newStringSet.contains("ab"));
-        assertTrue(newStringSet.contains("abc"));
+        stringSet.serialize(output);
+        output.close();
+        deserializedStringSet.deserialize(input);
+        assertTrue(deserializedStringSet.contains(""));
+        assertTrue(deserializedStringSet.contains("a"));
+        assertFalse(deserializedStringSet.contains("ab"));
+        assertTrue(deserializedStringSet.contains("abc"));
+        assertEquals(stringSet.size(), deserializedStringSet.size());
     }
 
 
     @Test(expected = SerializationException.class)
-    public void testSerializationFails() {
-        byte[] buf = {0};
-        final ByteArrayInputStream inputStream = new ByteArrayInputStream(buf);
-        final StringSetImpl newStringSet = new StringSetImpl();
-        newStringSet.deserialize(inputStream);
+    public void testSerializationFails() throws IOException {
+        output.close();
+        deserializedStringSet.deserialize(input);
         fail();
     }
 }
