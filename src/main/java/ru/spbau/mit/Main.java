3fcaf5b1359110de5d7672c46d9a5488d19c6172
mchernyavsky <chemike47@gmail.com>
2017-03-19 22:18:20 +0300

    Fix style #2

--- src/main/java/ru/spbau/mit/Main.java
+++ src/main/java/ru/spbau/mit/Main.java
@@ -3,7 +3,7 @@ package ru.spbau.mit;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class Main {
+public final class Main {
     private static final Logger LOG = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
