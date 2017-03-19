9d475eddd170cd622130226ffc59f9d729833060
mchernyavsky <chemike47@gmail.com>
2017-03-19 22:12:47 +0300

    Add logging

---
+++ src/main/java/ru/spbau/mit/Main.java
@@ -0,0 +1,17 @@
+package ru.spbau.mit;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    private static final Logger LOG = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOG.trace("Trace Message!"); // ignore
+        LOG.debug("Debug Message!"); // ignore
+        LOG.info("Info Message!");   // console
+        LOG.warn("Warn Message!");   // console
+        LOG.error("Error Message!"); // file
+        LOG.fatal("Fatal Message!"); // console
+    }
+}
