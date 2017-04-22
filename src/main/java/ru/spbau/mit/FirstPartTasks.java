4bb041c2079ff1f12482c2d2516bf95eb3e7edf1
misha <chemike47@gmail.com>
2017-04-22 23:12:39 +0300

    sudo fix style

--- src/main/java/ru/spbau/mit/FirstPartTasks.java
+++ src/main/java/ru/spbau/mit/FirstPartTasks.java
@@ -37,11 +37,11 @@ public final class FirstPartTasks {
 
     // Список альбомов, в которых есть хотя бы один трек с рейтингом более 95, отсортированный по названию
     public static List<Album> sortedFavorites(Stream<Album> albums) {
-        final int MAGIC_CONST = 95;
+        final int threshold = 95;
         return albums
                 .filter(album -> album.getTracks().stream()
                         .map(Track::getRating)
-                        .anyMatch(rating -> rating > MAGIC_CONST)
+                        .anyMatch(rating -> rating > threshold)
                 )
                 .sorted(Comparator.comparing(Album::getName))
                 .collect(Collectors.toList());
