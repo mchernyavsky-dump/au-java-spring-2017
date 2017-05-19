aa1b9f8207b7b1b91e04014604115da0b3eb057b
misha <chemike47@gmail.com>
2017-05-19 11:06:52 +0300

    Implement Injector?

--- src/main/java/ru/spbau/mit/Injector.java
+++ src/main/java/ru/spbau/mit/Injector.java
@@ -1,6 +1,8 @@
 package ru.spbau.mit;
 
-import java.util.List;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.*;
 
 
 public final class Injector {
@@ -11,7 +13,74 @@ public final class Injector {
      * Create and initialize object of `rootClassName` class using classes from
      * `implementationClassNames` for concrete dependencies.
      */
-    public static Object initialize(String rootClassName, List<String> implementationClassNames) throws Exception {
-        throw new IllegalStateException();
+    public static Object initialize(
+            final String rootClassName,
+            final List<String> implementationClassNames
+    ) throws Exception {
+        final Class<?> rootClass = Class.forName(rootClassName);
+        final Set<Class<?>> implementationClasses = new HashSet<>();
+        for (String implementationClass : implementationClassNames) {
+            final Class<?> clazz = Class.forName(implementationClass);
+            implementationClasses.add(clazz);
+        }
+        return newInstance(rootClass, implementationClasses, new HashSet<>(), new HashSet<>());
+    }
+
+    private static Object newInstance(
+            final Class<?> clazz,
+            final Set<Class<?>> implementationClasses,
+            final Set<Class<?>> dependencies,
+            final Set<Object> cache
+    ) throws IllegalAccessException, InvocationTargetException, InstantiationException,
+            AmbiguousImplementationException, ImplementationNotFoundException, InjectionCycleException {
+        if (!dependencies.add(clazz)) {
+            throw new InjectionCycleException();
+        }
+
+        for (Object impl : cache) {
+            if (clazz.isAssignableFrom(impl.getClass())) {
+                return impl;
+            }
+        }
+
+        final Constructor<?> constructor = clazz.getConstructors()[0];
+        final List<Object> initargs = new ArrayList<>();
+        for (Class<?> paramClazz : constructor.getParameterTypes()) {
+            final Object param = implementationClasses.contains(paramClazz)
+                    ? newInstance(paramClazz, implementationClasses, dependencies, cache)
+                    : assignableNewInstance(paramClazz, implementationClasses, dependencies, cache);
+            cache.add(param);
+            initargs.add(param);
+        }
+
+        final Object instance = constructor.newInstance(initargs.toArray());
+        cache.add(instance);
+        dependencies.remove(clazz);
+        return instance;
+    }
+
+    private static Object assignableNewInstance(
+            final Class<?> clazz,
+            final Set<Class<?>> implementationClasses,
+            final Set<Class<?>> dependencies,
+            final Set<Object> cache
+    ) throws IllegalAccessException, InvocationTargetException, InstantiationException,
+            AmbiguousImplementationException, ImplementationNotFoundException, InjectionCycleException {
+        Class<?> currentClassImpl = null;
+        for (Class<?> classImpl : implementationClasses) {
+            if (clazz.isAssignableFrom(classImpl)) {
+                if (currentClassImpl == null) {
+                    currentClassImpl = classImpl;
+                } else {
+                    throw new AmbiguousImplementationException();
+                }
+            }
+        }
+
+        if (currentClassImpl == null) {
+            throw new ImplementationNotFoundException();
+        }
+
+        return newInstance(currentClassImpl, implementationClasses, dependencies, cache);
     }
 }
